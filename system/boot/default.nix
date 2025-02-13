f346b47ec2866c17a0b1e7e200ab6b6074a6e441
myamusashi <jinxprogilang666@gmail.com>
2025-02-13 20:50:24 +0700

    Remove kernel packages (tkg-pds) and configure sysctl to better performance

--- system/boot/default.nix
+++ system/boot/default.nix
@@ -3,54 +3,12 @@
   lib,
   ...
 }: {
-  # boot.kernelPackages = let
-  #   patchDir = ./extra-packages/kernel-patch;
-  #
-  #   # Baca isi direktori dan filter file .patch
-  #   patchFiles =
-  #     lib.mapAttrsToList
-  #     (name: type: {
-  #       inherit name;
-  #       path = patchDir + "/${name}";
-  #     })
-  #     (lib.filterAttrs
-  #       (name: type: type == "regular" && lib.hasSuffix ".patch" name)
-  #       (builtins.readDir patchDir));
-  #
-  #   customConfigs = import ./extra-packages/kernel-config.nix {inherit lib;};
-  #   linux_tkg_pds_pkg = {
-  #     fetchurl,
-  #     buildLinux,
-  #     ...
-  #   } @ args:
-  #     buildLinux (args
-  #       // rec {
-  #         version = "6.13.1";
-  #         modDirVersion = version;
-  #
-  #         src = fetchurl {
-  #           url = "https://www.kernel.org/pub/linux/kernel/v6.x/linux-${version}.tar.xz";
-  #           hash = "sha256-8BH2yOpHHfGz273R6yYbKckuQzYFA8Pr0AW+7CFVtmo=";
-  #         };
-  #         # kernelPatches = lib.forEach patchFiles (file: {
-  #         #   name = "patch-${lib.removeSuffix ".patch" file.name}";
-  #         #   patch = file.path;
-  #         # });
-  #
-  #         extraStructureConfig = customConfigs;
-  #
-  #         extraMeta.branch = "linux-tkg-pds-${version}";
-  #       }
-  #       // (args.argsOverride or {}));
-  #   linux_tkg_pds = pkgs.callPackage linux_tkg_pds_pkg {};
-  # in
-  #   pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_tkg_pds);
   boot.kernelPackages = pkgs.linuxPackages_cachyos;
   boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "usbhid" "sd_mod" "rtsx_pci_sdmmc"];
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.supportedFilesystems = ["ntfs"];
-  boot.kernelModules = ["kvm-intel" "uas" "usbhid" "hid" "usb_storage" "zram" ];
+  boot.kernelModules = ["kvm-intel" "uas" "usbhid" "hid" "usb_storage" "zram"];
   boot.kernelParams = ["console=tty1" "noibrs" "noibpb" "nopti" "nospectre_v2" "nospectre_v1" "l1tf=off" "nospec_store_bypass_disable" "no_stf_barrier" "mds=off" "mitigations=off"];
 
   boot.kernel.sysctl = {
@@ -74,5 +32,9 @@
     "net.ipv4.tcp_keepalive_time" = 60;
     "net.ipv4.tcp_keepalive_intvl" = 10;
     "net.ipv4.tcp_keepalive_probes" = 6;
+
+    "vm.vfs_cache_pressure" = 50;
+    "vm.dirty_ratio" = 10;
+		"vm.swappiness" = 35;
   };
 }
