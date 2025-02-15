029189fdeb85c4e4f588a58b96f8b0802b47cfd7
myamusashi <jinxprogilang666@gmail.com>
2025-02-16 04:22:57 +0700

    yazi: add more plugin

--- home/programs/yazi/plugins/package.nix
+++ home/programs/yazi/plugins/package.nix
@@ -12,61 +12,81 @@
     rev = "2c3f675d3722672eb3a3422590375d1145843a40";
     hash = "sha256-LKO2kbpI8NnEnvIIAp01RefSlhGQV1BRDNO03eLQZoA=";
   };
-	
+
+  eza-preview = pkgs.fetchFromGitHub {
+    owner = "ahkohd";
+    repo = "eza-preview.yazi";
+    rev = "5ef05bcee141291566276e62cc16e265a387dca4";
+    hash = "sha256-L7i+uL2kAx3AUr5EAzRrduoV2m4+/tE1gCfbTOSuAc4=";
+  };
+
   glow = pkgs.fetchFromGitHub {
     owner = "Reledia";
     repo = "glow.yazi";
     rev = "5ce76dc92ddd0dcef36e76c0986919fda3db3cf5";
     hash = "sha256-UljcrXXO5DZbufRfavBkiNV3IGUNct31RxCujRzC9D4=";
   };
-	
+
   hexyl = pkgs.fetchFromGitHub {
     owner = "Reledia";
     repo = "hexyl.yazi";
     rev = "39d3d4e23ad7cec8888f648ddf55af4386950ce7";
     hash = "sha256-nsnnL3GluKk/p1dQZTZ/RwQPlAmTBu9mQzHz1g7K0Ww=";
   };
-	
+
   ouch = pkgs.fetchFromGitHub {
     owner = "ndtoan96";
     repo = "ouch.yazi";
     rev = "ce6fb75431b9d0d88efc6ae92e8a8ebb9bc1864a";
     hash = "sha256-oUEUGgeVbljQICB43v9DeEM3XWMAKt3Ll11IcLCS/PA=";
   };
-	
+
   rich-preview = pkgs.fetchFromGitHub {
     owner = "AnirudhG07";
     repo = "rich-preview.yazi";
     rev = "2559e5fa7c1651dbe7c5615ef6f3b5291347d81a";
     hash = "sha256-dW2gAAv173MTcQdqMV32urzfrsEX6STR+oCJoRVGGpA=";
   };
-	
+
   yatline = pkgs.fetchFromGitHub {
     owner = "imsi32";
     repo = "yatline";
     rev = "93282055cfb4d74a9bc8e47cf2035ded9b3c5b00";
     hash = "sha256-1TodD0sIeb/Y24rJk7pd1IfqLmbsFzxWvV0RSfwabIA=";
   };
-	
+
   yatline-symlink = pkgs.fetchFromGitHub {
     owner = "lpanebr";
     repo = "yazi-plugins";
     rev = "ca18a2cfb893e3608997c9de54acced124373871";
     hash = "sha256-v+EmMbrccAlzeR9rhmlFq0f+899l624EhWx1DFz+qzc=";
   };
-	
+
   lazygit = pkgs.fetchFromGitHub {
     owner = "Lil-Dank";
     repo = "lazygit.yazi";
     rev = "9f924e34cde61d5965d6d620698b0b15436c8e08";
     hash = "sha256-ns9DzIdI2H3IuCByoJjOtUWQQB9vITxmJ/QrYt+Rdao=";
   };
-	
+
   githead = pkgs.fetchFromGitHub {
     owner = "llanosrocas";
     repo = "githead.yazi";
     rev = "23750247d59da485e4b5257ad5e83837bb86a685";
     hash = "sha256-VErtv5+I3BBnfbOQrHFvvLo7GAExXwVmhHNq+84kam8=";
   };
-	
+
+  kanagawa = pkgs.fetchFromGitHub {
+    owner = "dangooddd";
+    repo = "kanagawa.yazi";
+    rev = "d98f0c3e27299f86ee080294df2722c5a634495a";
+    hash = "sha256-Z/lyXNCSqX0wvCQd39ZedxOGlhRzQ+M0hqzkBEcpxEE=";
+  };
+
+	fzf = pkgs.fetchFromGitHub {
+	  owner = "DreamMaoMao";
+	  repo = "fg.yazi";
+	  rev = "daf696065d65e61a1b3026ab8190351203513d51";
+	  hash = "sha256-dcidPBhc0+NvPb80hK+kUoq+PxspceFCliyEc7K3OTk=";
+	};
 }
--- home/programs/yazi/yazi.nix
+++ home/programs/yazi/yazi.nix
@@ -1,30 +1,194 @@
 {pkgs, ...}: let
-  yazi-plugins = import ./plugins.nix {inherit pkgs;};
+  yazi-plugins = import ./plugins/package.nix {inherit pkgs;};
 in {
+  home.packages = with pkgs; [
+    mediainfo
+    ouch
+  ];
   programs.yazi = {
     enable = true;
-    # keymap = {
-    # 	manager.prepend_keymap= [
-    # 		{ run = "plugin eza-preview", desc = ""}
-    # 	];
-    # };
+    initLua = ./init.lua;
+    flavors = {
+      kanagawa = "${yazi-plugins.kanagawa}";
+    };
+    theme.flavor = {
+      dark = "kanagawa";
+    };
     plugins = {
-      chmod = "${yazi-plugins.chmod}/chmod.yazi";
-      diff = "${yazi-plugins.diff}/diff.yazi";
-      full-border = "${yazi-plugins.full-border}/full-border.yazi";
-      git = "${yazi-plugins.git}/git.yazi";
-      hide-preview = "${yazi-plugins.hide-preview}/hide-preview.yazi";
-      mount = "${yazi-plugins.mount}/mount.yazi";
-      mime-ext = "${yazi-plugins.mime-ext}/mime-ext.yazi";
+      chmod = "${yazi-plugins.official-plugins}/chmod.yazi";
+      diff = "${yazi-plugins.official-plugins}/diff.yazi";
+      full-border = "${yazi-plugins.official-plugins}/full-border.yazi";
+      git = "${yazi-plugins.official-plugins}/git.yazi";
+      hide-preview = "${yazi-plugins.official-plugins}/hide-preview.yazi";
+      mount = "${yazi-plugins.official-plugins}/mount.yazi";
+      mime-ext = "${yazi-plugins.official-plugins}/mime-ext.yazi";
+      eza-preview = "${yazi-plugins.eza-preview}";
       mediainfo = "${yazi-plugins.mediainfo}";
+      fg = "${yazi-plugins.fzf}";
       glow = "${yazi-plugins.glow}";
       hexyl = "${yazi-plugins.hexyl}";
       ouch = "${yazi-plugins.ouch}";
-      rich-preview = "${yazi-plugins.rich-preview}";
       yatline = "${yazi-plugins.yatline}";
       yatline-symlink = "${yazi-plugins.yatline-symlink}/yatline-symlink.yazi";
       lazygit = "${yazi-plugins.lazygit}";
       githead = "${yazi-plugins.githead}";
+    };
+    keymap = {
+      manager.prepend_keymap = [
+        {
+          on = ["E"];
+          run = "plugin eza-preview";
+          desc = "Toggle tree/list dir preview";
+        }
+        {
+          on = ["-"];
+          run = "plugin eza-preview --args='--inc-level'";
+          desc = "Increment tree level";
+        }
+        {
+          on = ["_"];
+          run = "plugin eza-preview --args='--dec-level'";
+          desc = "Decrement tree level";
+        }
+        {
+          on = ["$"];
+          run = "plugin eza-preview --args='--toggle-follow-symlinks'";
+          desc = "Toggle tree follow symlinks";
+        }
+
+        {
+          on = ["<C-d>"];
+          run = "plugin diff";
+          desc = "Diff the selected with the hovered file";
+        }
+        {
+          on = ["g" "i"];
+          run = "plugin lazygit";
+          desc = "run lazygit";
+        }
+        {
+          on = ["T"];
+          run = "plugin hide-preview";
+          desc = "Hide or show preview";
+        }
+        {
+          on = ["M"];
+          run = "plugin mount";
+        }
+        {
+          on = ["<C-e>"];
+          run = "seek 5";
+        }
+        {
+          on = ["<C-y>"];
+          run = "seek -5";
+        }
+        {
+          on = ["f" "g"];
+          run = "plugin fg";
+          desc = "find file by content";
+        }
+        {
+          on = ["f" "f"];
+          run = "plugin fg --args='fzf'";
+          desc = "find file by filename";
+        }
+        {
+          on = ["f" "G"];
+          run = "plugin fg --args='rg'";
+          desc = "find file by content (ripgrep match)";
+        }
+        {
+          on = ["C"];
+          run = "plugin ouch --args=zip";
+          desc = "Compress with ouch";
+        }
+      ];
+    };
+    settings = {
+      opener.extract = [
+        {
+          run = "ouch d -y '$@'";
+          desc = "Extract here with ouch";
+          for = "unix";
+        }
+      ];
+      plugin.append_previewers = [
+        {
+          name = "*";
+          run = "hexyl";
+        }
+      ];
+      plugin.prepend_fetchers = [
+        {
+          id = "git";
+          name = "*";
+          run = "git";
+        }
+        {
+          id = "git";
+          name = "*/";
+          run = "git";
+        }
+        {
+          id = "mime";
+          name = "*";
+          run = "mime-ext";
+          prio = "high";
+        }
+      ];
+      plugin.prepend_preloaders = [
+        {
+          mime = "{audio,video,image}/*";
+          run = "mediainfo";
+        }
+        {
+          mime = "application/subrip";
+          run = "mediainfo";
+        }
+      ];
+      plugin.prepend_previewers = [
+        {
+          name = "*/";
+          run = "eza-preview";
+        }
+        {
+          mime = "{audio,video,image}/*";
+          run = "mediainfo";
+        }
+        {
+          mime = "application/subrip";
+          run = "mediainfo";
+        }
+        {
+          name = "*.md";
+          run = "glow";
+        }
+        {
+          mime = "application/*zip";
+          run = "ouch";
+        }
+        {
+          mime = "application/x-tar";
+          run = "ouch";
+        }
+        {
+          mime = "application/x-bzip2";
+          run = "ouch";
+        }
+        {
+          mime = "application/x-7z-compressed";
+          run = "ouch";
+        }
+        {
+          mime = "application/x-rar";
+          run = "ouch";
+        }
+        {
+          mime = "application/x-xz";
+          run = "ouch";
+        }
+      ];
     };
   };
 }
