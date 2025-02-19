ac41c8924ef85911c1eba5df8247b1fc291c1133
myamusashi <jinxprogilang666@gmail.com>
2025-02-19 18:49:57 +0700

    filesystems: fix ext4 external drive can't mount and have permission issue

--- system/config/modules/fs.nix
+++ system/config/modules/fs.nix
@@ -1,27 +1,32 @@
 {
-  fileSystems."/run/media/apalah" = {
-    device = "/dev/disk/by-label/apalah";
-    fsType = "ntfs-3g";
-    options = [
-      "rw"
-      "uid=1000"
-      "gid=100"
-      "umask=022"
-      "nofail"
-    ];
-		noCheck = true;
-  };
+  fileSystems = {
+    "/run/media/apalah" = {
+      device = "/dev/disk/by-label/apalah";
+      fsType = "ntfs-3g";
+      options = [
+        "rw"
+        "auto"
+        "users"
+        "uid=1000"
+        "gid=100"
+        "umask=022"
+        "nofail"
+      ];
+      noCheck = true;
+    };
 
-  fileSystems."/run/media/extn" = {
-    device = "/dev/disk/by-label/extn";
-    fsType = "ext4";
-    options = [
-      "rw"
-      "user"
-      "nofail"
-      "exec"
-      "umask=000"
-    ];
-		noCheck = true;
+    "/run/media/extn" = {
+      device = "/dev/disk/by-label/extn";
+      fsType = "ext4";
+      options = [
+        "rw"
+        "auto"
+        "users"
+        "nofail"
+        "exec"
+				"X-mount.owner=myamusashi"
+      ];
+      noCheck = true;
+    };
   };
 }
