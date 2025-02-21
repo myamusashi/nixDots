7f3d56fa0b7e34f68dc1c44e6731b9b6aa018ad6
myamusashi <jinxprogilang666@gmail.com>
2025-02-21 15:03:06 +0700

    programs: install vscode

---
+++ home/programs/vscode/vscode.nix
@@ -0,0 +1,39 @@
+{pkgs, ...}: {
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+    userSettings = {
+      workbench.iconTheme = "material-icon-theme";
+      workbench.colorTheme = "Material Theme Darker";
+      editor.minimap.enabled = false;
+
+      apc.imports = [
+        "file =///home/myamusashi/.vscode-oss/extensions/brandonkirbyson.vscode-animations-2.0.1-universal/dist/updateHandler.js"
+      ];
+
+      workbench.settings.applyToAllProfiles = [
+        "animations.Command-Palette"
+      ];
+
+      vscode_custom_css.imports = [
+        ""
+      ];
+
+      editor.cursorSmoothCaretAnimation = "on";
+      animations.CursorAnimation = true;
+      editor.fontFamily = "CaskaydiaMono Nerd Font";
+      editor.smoothScrolling = true;
+      editor.cursorBlinking = "smooth";
+      workbench.reduceMotion = "off";
+      terminal.integrated.smoothScrolling = true;
+      animations.Durations = {
+        Scrolling = 200;
+        Tabs = 300;
+        Smooth-Mode = 200;
+        Active = 200;
+        Focus-Dimming = 400;
+        Command-Palette = 1000;
+      };
+    };
+  };
+}
