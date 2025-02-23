719051c6d087489de781b2818969bd5d7b4f5003
myamusashi <jinxprogilang666@gmail.com>
2025-02-24 05:43:25 +0700

    services: fix greetd autologin make Hyprland stuck and consume a lot of memory and cpu

--- system/services/modules/greetd/default.nix
+++ system/services/modules/greetd/default.nix
@@ -2,14 +2,15 @@
   services.greetd = {
     enable = true;
     vt = 2;
-    settings = {
+    settings = rec {
       initial_session = {
-        command = "${pkgs.hyprland}/bin/Hyprland";
+        command = "Hyprland";
         user = "myamusashi";
       };
+      # default_session = initial_session;
       default_session = {
         command = ''
-          ${pkgs.greetd.tuigreet}/bin/tuigreet tuigreet --remember --remember-user-session --time --asterisks --asterisks-char "+" --greeting "Loh kok bisa"  --theme border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red --cmd Hyprland'';
+          ${pkgs.greetd.tuigreet}/bin/tuigreet tuigreet --remember --remember-user-session --time --asterisks --asterisks-char "+" --greeting "Loh kok bisa"  --theme border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red --cmd ${pkgs.sway}/bin/sway'';
         user = "greeter";
       };
     };
