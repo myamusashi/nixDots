754e71d67c36021e9e82a8eacc6aa76e0b383ae5
myamusashi <jinxprogilang666@gmail.com>
2025-02-25 05:55:31 +0700

    settings: add new fonts avenir-LT-pro

--- home/settings/fonts/fonts.nix
+++ home/settings/fonts/fonts.nix
@@ -1,8 +1,10 @@
 {pkgs, ...}: let
   feather-font = pkgs.callPackage ./packages/feather-font.nix {};
+	avenirLTPro-font = pkgs.callPackage ./packages/avenir-font.nix {};
 in {
   home.packages = [
     feather-font
+		avenirLTPro-font
     pkgs.nerd-fonts.caskaydia-mono
     pkgs.nerd-fonts.comic-shanns-mono
     pkgs.nerd-fonts.proggy-clean-tt
