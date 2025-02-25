2e09f57117aa0214af02abcd0ad86decafb252d1
myamusashi <jinxprogilang666@gmail.com>
2025-02-25 09:15:39 +0700

    settings: add more fonts for browser

--- home/settings/fonts/fonts.nix
+++ home/settings/fonts/fonts.nix
@@ -1,11 +1,14 @@
 {pkgs, ...}: let
   feather-font = pkgs.callPackage ./packages/feather-font.nix {};
-	avenirLTPro-font = pkgs.callPackage ./packages/avenir-font.nix {};
+  avenirLTPro-font = pkgs.callPackage ./packages/avenir-font.nix {};
 in {
   home.packages = [
     feather-font
-		avenirLTPro-font
+    avenirLTPro-font
+    pkgs.montserrat
+    pkgs.overpass
     pkgs.nerd-fonts.caskaydia-mono
+		pkgs.source-sans-pro
     pkgs.nerd-fonts.comic-shanns-mono
     pkgs.nerd-fonts.proggy-clean-tt
     pkgs.nerd-fonts.jetbrains-mono
