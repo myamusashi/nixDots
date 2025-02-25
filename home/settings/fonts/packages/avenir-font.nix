9acad1f89dca5e0d00672897623fa47401da3f9b
myamusashi <jinxprogilang666@gmail.com>
2025-02-25 09:15:19 +0700

    settings: fix avenir-font and add hash

--- home/settings/fonts/packages/avenir-font.nix
+++ home/settings/fonts/packages/avenir-font.nix
@@ -5,27 +5,21 @@
 }:
 stdenvNoCC.mkDerivation rec {
   pname = "avenir-lt-pro";
+  version = "2.0";
 
   src = fetchzip {
     url = "https://font.download/dl/font/${pname}.zip";
-    sha256 = "";
+    hash = "sha256-muTE39N6pTKgpiF+7ikAwycReBFCrnwf0Syik5Wddvs=";
+    stripRoot = false;
   };
 
-  postInstall = ''
-    mkdir -p $out/share/fonts/truetype/${pname}
+  installPhase = ''
+    runHook preInstall
 
-    install -Dm644 ./AvenirLTProBlack.otf $out/share/fonts/truetype/${pname}/AvenirLTProBlack.otf
-    install -Dm644 ./AvenirLTProBlackOblique.otf $out/share/fonts/truetype/${pname}/AvenirLTProBlackOblique.otf
-    install -Dm644 ./AvenirLTProBook.otf $out/share/fonts/truetype/${pname}/AvenirLTProBook.otf
-    install -Dm644 ./AvenirLTProBookOblique.otf $out/share/fonts/truetype/${pname}/AvenirLTProBookOblique.otf
-    install -Dm644 ./AvenirLTProHeavy.otf $out/share/fonts/truetype/${pname}/AvenirLTProHeavy.otf
-    install -Dm644 ./AvenirLTProHeavyOblique.otf $out/share/fonts/truetype/${pname}/AvenirLTProHeavyOblique.otf
-    install -Dm644 ./AvenirLTProLight.otf $out/share/fonts/truetype/${pname}/AvenirLTProLight.otf
-    install -Dm644 ./AvenirLTProLightOblique.otf $out/share/fonts/truetype/${pname}/AvenirLTProLightOblique.otf
-    install -Dm644 ./AvenirLTProMedium.otf $out/share/fonts/truetype/${pname}/AvenirLTProMedium.otf
-    install -Dm644 ./AvenirLTProMediumOblique.otf $out/share/fonts/truetype/${pname}/AvenirLTProMediumOblique.otf
-    install -Dm644 ./AvenirLTProOblique.otf $out/share/fonts/truetype/${pname}/AvenirLTProOblique.otf
-    install -Dm644 ./AvenirLTProRoman.otf $out/share/fonts/truetype/${pname}/AvenirLTProRoman.otf
+    mkdir -p $out/share/fonts/opentype/${pname}
+    install -Dm644 $src/*.otf -t $out/share/fonts/opentype/${pname}/
+
+    runHook postInstall
   '';
 
   meta = with lib; {
